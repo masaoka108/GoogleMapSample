2a811e0c969006acfbbcbb7dfd35cb2c70cf09bb
Masahiro Okamura <okamok108@gmail.com>
2018-04-08 13:40:57 +0900

    first commit

--- GoogleMapSample/AppDelegate.swift
+++ GoogleMapSample/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        GMSServices.provideAPIKey("AIzaSyBW6PV1d7u8hdJpSYBVP0Vt5hNKKNYYoTM")
+        GMSPlacesClient.provideAPIKey("AIzaSyBybFn3RPxUym_BVtHkCkYEPJS6pqzQ2KQ")
+
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+target 'GoogleMapSample' do
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+end
